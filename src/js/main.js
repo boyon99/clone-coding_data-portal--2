84cdb45e0104c932f28f35f76e683373a441ae7c
boyon99 <bo_yon_99@naver.com>
2022-12-29 17:43:53 +0900

    feat: add category-data-3 section

--- src/js/main.js
+++ src/js/main.js
@@ -44,6 +44,24 @@ var swiper = new Swiper(".mySwiper", {
   loop: true
 });
 
+
+/* news - swiper */
+var swiper = new Swiper(".mySwiper--2", {
+  pagination: {
+    el: ".swiper-pagination",
+    type: "fraction"
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
+  },
+  autoplay: {
+    delay : 3000
+  },
+  loop: true
+});
+
+
 /* date */
 
 date = new Date();
