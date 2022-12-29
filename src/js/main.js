5d06885545e9ef0936f662c4ed14613a827ae013
boyon99 <bo_yon_99@naver.com>
2022-12-29 14:35:24 +0900

    feat: add data--container to use tab
    텝 메뉴 여러 개 동시 구현

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,4 @@
+/* popular search - swiper */
 new Swiper('.search__popular__btn .swiper-container', {
   direction:'vertical',
   autoplay: {
@@ -8,3 +9,23 @@ new Swiper('.search__popular__btn .swiper-container', {
 
 
 
+/* data - tab */
+// 탭 컨텐츠 숨기기
+$(".data__content").hide();
+
+// 첫번째 탭콘텐츠 보이기
+$(".data__container").each(function () {
+  $(this).children(".data__name li:first").addClass("active"); //Activate first tab
+  $(this).children(".data__content").first().show();
+});
+//탭메뉴 클릭 이벤트
+$(".data__name li a").click(function () {
+  
+  $(this).parent().siblings("li").removeClass("active");
+  $(this).parent().addClass("active"); $(this).parent().parent().parent().parent().find(".data__content").hide();
+  var activeTab = $(this).attr("rel");
+   $("#" + activeTab).fadeIn();
+});
+
+
+/* recommand data - swiper */
